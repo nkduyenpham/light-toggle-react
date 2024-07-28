5aef228554ad7d251e5414ced765f9a468798747
nkduyenpham <90058312+nkduyenpham@users.noreply.github.com>
2024-07-28 23:16:05 +0800

    finish setup the toggle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Switch = ({ toggle, onClick }) => {
+    // basing on 'toggle' state
+    const switchClass = toggle ? "bg-gray-700 justify-end" : "bg-blue-300 justify-start";
+    const imgSrc = toggle ? "/moon.png" : "/sun.png";
+
+    return (
+        <motion.div animate className={`flex items-center w-[170px] h-[100px] rounded-full p-[10px] cursor-pointer z-2 transition duration-500 ease-in-out ${switchClass}`} onClick={onClick}>
+          <motion.div animate className="w-[80px] h-[80px] rounded-full overflow-hidden">
+            <img src={imgSrc} alt="Toggle" />
+          </motion.div>
+        </motion.div>
+    );
+};
+
+const App = () => {
+    // State to track the on/off status of the switch
+    const [toggle, setToggle] = useState(false);
+
+    useEffect(() => {
+        // Change the background color of the body based on `toggle` state
+        document.body.style.backgroundColor = toggle ? "#1c1c1c" : "#f1ebe1";
+    }, [toggle]);
+
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <Switch
+                toggle={toggle}
+                onClick={() => setToggle(!toggle)}
+            />
+        </div>
+    );
+};
 
 export default App;
